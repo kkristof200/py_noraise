9a542ee551bd17e13d4e5366bd0a8dfa438e879f
kkristof200 <kovacskristof200@gmail.com>
2021-04-18 00:19:49 +0300

    Update

--- demo.py
+++ demo.py
@@ -1,8 +1,25 @@
 from noraise import noraise
 
-@noraise(print_exc=True)
+@noraise(print_exc=True, return_exception=True)
 def f2():
     return 1/0
 
 res = f2()
 print(type(res), res)
+
+# This will not catch the crash, and print
+# 
+# 
+# < ----------------------------------------------- Caught with @noraise ---------------------------------------------- >
+# 
+# Traceback (most recent call last):
+#   File "/Users/kristofk/github/py_noraise/noraise/noraise.py", line 32, in wrapper
+#     return function(*args, **kwargs)
+#   File "demo.py", line 5, in f2
+#     return 1/0
+# ZeroDivisionError: division by zero
+# 
+# < -------------------------------------------------------------------------------------------------------------------- >
+# 
+# 
+# <class 'ZeroDivisionError'> division by zero
